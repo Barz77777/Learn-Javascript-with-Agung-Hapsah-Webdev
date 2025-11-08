ac095cbe3f43910373735b5a7de3b5f0b389bb16
makbarenurhermawan@gmail.com <makbarenurhermawan@gmail.com>
2025-11-08 15:18:04 +0700

    Update

--- app.js
+++ app.js
@@ -1,3 +1,49 @@
+// Challenge :
+// Buat 3 variable.
+// 1 untuk nama depan kamu
+// 1 untuk nama belakang kamu 
+// 1 untuk nama lengkap kamu
+// Log masing - masing variable
+const myFirstName = 'Muhammad Akbar';
+const myFavoritFood = 'Telur campur kecap';
+const myOffice = 'Vistakom';
+
+const text = 'Halo namaku ' + myFirstName + ' dan makanan favoritku adalah ' + myFavoritFood + ' bekerja di ' + myOffice
+
+console.log(text);
+
+
+// Part 7 - Number
+// Challenge
+// Kamu penjual buah dengan price list berikut:
+// Apel = Rp5.000
+// Pisang = Rp10.000
+// Ada kustomer membeli 3 apel dan 2 pisang. Dia juga punya kupon diskon Rp10.000.
+// Hitung dan log total harga.
+
+const apel = 5000;
+const pisang = 10000;
+
+const beliApel = (apel * 3) + (pisang * 2);
+
+const Total = beliApel - 10000;
+
+console.log(Total)
+
+// Challenge - 2 - Floating point
+// Kamu penjual buah dengan price list berikut:
+// Apel = Rp5.000
+// Pisang = Rp10.000
+// Ada kustomer membeli 3 apel dan 2 pisang. Dia juga punya kupon diskon 10%
+// Hitung dan log total harga.
+const priceApel = 5000;
+const pricePisang = 10000;
+const totalApel = 3;
+const totalPisang = 2;
+const discount = 0.1;
+
+const originalPrice = priceApel * totalApel + pricePisang * totalPisang;
+const price = originalPrice - originalPrice * discount;
 
 // Challenge :
 // Buat 3 variable.
@@ -5,7 +51,6 @@
 // 1 untuk nama belakang kamu 
 // 1 untuk nama lengkap kamu
 // Log masing - masing variable
-
 
 const myFirstName = 'Muhammad Akbar';
 const myFavoritFood = 'Telur campur kecap';
@@ -48,54 +93,4 @@ const discount = 0.1;
 const originalPrice = priceApel * totalApel + pricePisang * totalPisang;
 const price = originalPrice - originalPrice * discount;
 
-=======
-// Challenge :
-// Buat 3 variable.
-// 1 untuk nama depan kamu
-// 1 untuk nama belakang kamu 
-// 1 untuk nama lengkap kamu
-// Log masing - masing variable
-
-const myFirstName = 'Muhammad Akbar';
-const myFavoritFood = 'Telur campur kecap';
-const myOffice = 'Vistakom';
-
-const text = 'Halo namaku ' + myFirstName + ' dan makanan favoritku adalah ' + myFavoritFood + ' bekerja di ' + myOffice
-
-console.log(text);
-
-
-// Part 7 - Number
-// Challenge
-// Kamu penjual buah dengan price list berikut:
-// Apel = Rp5.000
-// Pisang = Rp10.000
-// Ada kustomer membeli 3 apel dan 2 pisang. Dia juga punya kupon diskon Rp10.000.
-// Hitung dan log total harga.
-
-const apel = 5000;
-const pisang = 10000;
-
-const beliApel = (apel * 3) + (pisang * 2);
-
-const Total = beliApel - 10000;
-
-console.log(Total)
-
-// Challenge - 2 - Floating point
-// Kamu penjual buah dengan price list berikut:
-// Apel = Rp5.000
-// Pisang = Rp10.000
-// Ada kustomer membeli 3 apel dan 2 pisang. Dia juga punya kupon diskon 10%
-// Hitung dan log total harga.
-const priceApel = 5000;
-const pricePisang = 10000;
-const totalApel = 3;
-const totalPisang = 2;
-const discount = 0.1;
-
-const originalPrice = priceApel * totalApel + pricePisang * totalPisang;
-const price = originalPrice - originalPrice * discount;
-
->>>>>>> cbefe32 (First Commit)
 console.log(price)
