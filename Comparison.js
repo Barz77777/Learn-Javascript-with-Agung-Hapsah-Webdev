c3cb4ac43b5b75f6af3cdf668140f540afb18059
makbarenurhermawan@gmail.com <makbarenurhermawan@gmail.com>
2025-11-10 21:03:18 +0700

    learn comparison in JS

---
+++ Comparison.js
@@ -0,0 +1,72 @@
+//Part - 10 
+//Challenge 
+//Apa hasil perbandingan berikut:
+//1. 10 > 5
+//2. 8 < 4
+//3. -1  >= 2
+//4. 0.5 <= 0
+
+const satu = 10 > 5
+const dua = 8 < 4
+const tiga = -1 >= 2
+const empat = 0.5 <= 0
+
+if (satu) {
+    console.log('benar');
+} else {
+    console.log('salah');
+}
+
+if (dua) {
+    console.log('benar');
+} else {
+    console.log('salah');
+}
+
+if (tiga) {
+    console.log('benar');
+} else {
+    console.log('salah');
+}
+
+if (empat) {
+    console.log('benar');
+} else {
+    console.log('salah');
+}
+
+//Challenge - 2
+//Apa hasil perbandingan berikut:
+//1. 'abc' === 'abc'
+//2, false !== true 
+//3. 'cab' === 'cba'
+//4. false === false
+
+const lima = 'abc' === 'abc'
+const enam = false !== true
+const tujuh = -'cab' === 'cba'
+const delapan = false === false
+
+if (lima) {
+    console.log('true');
+} else {
+    console.log('false');
+}
+
+if (enam) {
+    console.log('true');
+} else {
+    console.log('false');
+}
+
+if (tujuh) {
+    console.log('true');
+} else {
+    console.log('false');
+}
+
+if (delapan) {
+    console.log('true');
+} else {
+    console.log('false');
+}
